da1eac5cc9cbffecc4eb34d19c77d248452fa5f7
Georg Leschnak <12657659+GeorgLHH@users.noreply.github.com>
2018-09-10 10:35:51 +0200

    EfCoreCodeFirst: Solition finished with an unresolved exception.

---
+++ EfCoreCodeFirst/EfCoreCodeFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EfCoreCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                //int count = db.SaveChanges();
+                int count = 5;
+                // following line causes an exception with the message that Blogs not exist in current context.
+                db.SaveChanges();
+                Console.WriteLine($"{count} records saved to database.");
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
